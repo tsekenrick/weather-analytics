378340515855da797da2fa49fc1fa7f68609d0b6
tsekenrick <kt1556@nyu.edu>
2019-02-18 13:52:24 -0500

    part 2 complete!

--- src/readWeatherFs.js
+++ src/readWeatherFs.js
@@ -1,2 +1,26 @@
 // require fs 
 // require tempAnalytic.js
+const fs = require('fs');
+const ta = require('./tempAnalytic.js');
+
+let content;
+fs.readFile('./historical-hourly-weather-data-json/temperature.json', 'utf8', (err, data) => {
+    if(err){
+        console.log('error');
+    }
+
+    // parse data into res array and convert K to F
+    content = JSON.parse(data);
+    const res = content.map(function(obj){
+        const ret = {};
+        Object.entries(obj).map(function(pair) {
+            if(pair[0] === 'datetime') {
+                ret[pair[0]] = pair[1];
+            } else {
+                ret[pair[0]] = pair[1] * 1.8 - 459.67;
+            }
+        });
+        return ret;
+    });
+    console.log(ta.analyzeTemperature(res));
+});
--- src/tempAnalytic.js
+++ src/tempAnalytic.js
@@ -8,26 +8,26 @@ function analyzeTemperature(weatherData){
     retStr += "The first 10 lines of Temperature in NY:\n";
     let count = 0;
     weatherData.map(function(obj){
-        count += 1;
         const time = obj.datetime;
         const filtered = Object.entries(obj).filter((pair) => pair[0] === 'New York');
         if(count < 10){
             filtered.map(pair => retStr += `At ${time}, the temperature in NY is ${pair[1].toPrecision(4)} (F)\n`);
-        }    
+        }  
+        count += 1;  
     });
     
     // Mean temp in San Diego
     const mean = weatherData.reduce(function(acc, cur){
         return acc + cur['San Diego'];
     }, 0);
-    retStr += `\nThe mean temperature in San Diego is: ${(mean/weatherData.length).toPrecision(4)}\n\n`;
+    retStr += `\nThe mean temperature in San Diego is: ${(mean/weatherData.length).toPrecision(4)} (F)\n\n`;
 
     // Min/max of temp in New York
     const min = weatherData.reduce(function(acc, cur){
         return acc[0] >= cur['New York'] ? [cur['New York'], cur['datetime']] : acc;
     }, [Infinity, undefined]);
     retStr += `The coldest time in New York is ${min[1]}\n`;
-    retStr += `The lowest temperature is: ${min[0].toPrecision(4)} (F)\n`;
+    retStr += `The lowest temperature is: ${min[0].toPrecision(3)} (F)\n`;
     
     const max = weatherData.reduce(function(acc, cur){
         return acc[0] <= cur['New York'] ? [cur['New York'], cur['datetime']] : acc;
@@ -59,13 +59,12 @@ function analyzeTemperature(weatherData){
     retStr += "\nTop 10 Cities with highest mean temperature\n";
     count = 0;
     meanTemps.map(temp => {
-        count += 1;
         // find location in tempSums matching temp
         if(count < 10){
             const location = Object.keys(tempSums).find(key => tempSums[key] === temp);
             retStr += `${location}: ${temp.toPrecision(4)} (F)\n`;
         }
-        
+        count += 1;
     });
     
     // get coolest 10
@@ -73,53 +72,30 @@ function analyzeTemperature(weatherData){
     retStr += "\nTop 10 Cities with lowest mean temperature\n";
     count = 0;
     meanTemps.map(temp => {
-        count += 1;
         // find location in tempSums matching temp
         if(count < 10){
             const location = Object.keys(tempSums).find(key => tempSums[key] === temp);
             retStr += `${location}: ${temp.toPrecision(4)} (F)\n`;
         }
-        
+        count += 1;
     });
 
     // Average New York temp in Spr 13
-    
+    const sprTempSum = weatherData.reduce((acc, cur) => {
+        const date = new Date(cur['datetime']);
+        if(date.getFullYear() === 2013 && date.getMonth() > 0 && date.getMonth() < 4){
+            acc[0] += cur['New York'];
+            acc[1] += 1;
+        }
+        return acc;
+    }, [0, 0]);
+
+    retStr += `\nThe average temperature over spring 2013 in New York is: ${(sprTempSum[0]/sprTempSum[1]).toPrecision(4)} (F) \n`;
+
     return retStr;
 }
 
-let content;
-fs.readFile('./historical-hourly-weather-data-json/temperature.json', 'utf8', (err, data) => {
-    if(err){
-        console.log('error');
-    }
 
-    // parse data into res array and convert K to F
-    content = JSON.parse(data);
-    const res = content.map(function(obj){
-        const ret = {};
-        Object.entries(obj).map(function(pair) {
-            if(pair[0] === 'datetime') {
-                ret[pair[0]] = pair[1];
-            } else {
-                ret[pair[0]] = pair[1] * 1.8 - 459.67;
-            }
-        });
-        return ret;
-    });
-    const strReport = analyzeTemperature(res);
-    console.log(strReport);
-    // demo of traversing data in res
-    // let count = 0;
-    // res.map(function(obj){
-    //     count += 1;
-    //     const time = obj.datetime;
-    //     const filtered = Object.entries(obj).filter((pair) => pair[0] === 'New York');
-    //     if(count < 10){
-    //         filtered.map(pair => console.log(`At ${time}, temp in NY is ${pair[1].toPrecision(4)} (F)`));
-    //     }
-        
-    // });
-});
 
 module.exports = {
     analyzeTemperature
